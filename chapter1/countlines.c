0843f531b31f3b02edfe9e3706debec1403f8635
Vadim <deadmoro3@gmail.com>
2011-05-21 10:54:37 +0300

    chapter 1.5.3 - count lines

---
+++ chapter1/countlines.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main() {
+    int c, nl;
+
+    nl = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n') {
+            ++nl;
+        }
+    }
+    printf("%d\n", nl);
+}
