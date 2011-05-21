5e20b5593816e81a64416689bc13ff9c25b1ed23
Vadim <deadmoro3@gmail.com>
2011-05-21 11:21:35 +0300

    Chapter 1.5.3 - exercise 1.10

---
+++ chapter1/ex_1-5-3_110.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            putchar('\\');
+            putchar('t');
+        } else if (c == '\b') {
+            putchar('\\');
+            putchar('b');
+        } else if (c == '\\') {
+            putchar('\\');
+            putchar('\\');
+        } else {
+            putchar(c);
+        }
+    }
+}
--- chapter1/ex_1-5-3_19.c
+++ chapter1/ex_1-5-3_19.c
@@ -4,7 +4,7 @@
 #define FALSE 0
 
 main() {
-    int c, last_char_blank;
+    int c, last_char_blank = FALSE;
 
     while ((c = getchar()) != EOF) {
         if (c == ' ' && last_char_blank == TRUE) {
