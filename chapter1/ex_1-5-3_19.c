cdf4bcfd525584312437e106c6b1270c987edbbf
Vadim <deadmoro3@gmail.com>
2011-05-21 11:10:37 +0300

    Chapter 1.5.3 - exercise 1.9

---
+++ chapter1/ex_1-5-3_19.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+main() {
+    int c, last_char_blank;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' && last_char_blank == TRUE) {
+            continue;
+        } else if (c == ' ') {
+            last_char_blank = TRUE;
+        } else {
+            last_char_blank = FALSE;
+        }
+
+        putchar(c);
+    }
+}
